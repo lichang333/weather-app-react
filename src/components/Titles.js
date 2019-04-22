e935684e8c8d4ca727ae45478990330e41e178fc
lichang333 <lichang@oregonstate.edu>
2019-04-22 21:57:02 +0800

    components

---
+++ src/components/Titles.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+class Titles extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>天气查询器</h1>
+        <p>Find out temperature, conditions and more...</p>
+      </div>
+    );
+  }
+};
+
+export default Titles;
